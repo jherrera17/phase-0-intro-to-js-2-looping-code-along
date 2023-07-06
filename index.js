6c414dff0461f0326f5670d39a4763909540c410
Jose Herrera <jdj.herrera27@gmail.com>
2023-07-06 04:05:55 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const array = [];
+
+function writeCards(array, event) {
+    const newArray = []; {
+        for (let i =0; i < array.length; i++) {
+            newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+        }
+    }
+    return newArray;
+}
+
+console.log("Hello")
+
+function countDown(count){
+    while (count >= 0) {
+        console.log(count--);
+    }
+}
+
+countDown(10);
